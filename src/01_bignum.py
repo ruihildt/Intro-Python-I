9f620b2af77b47398ea2fc80d90b8cfdebf6bdd8
rui hildt <ruihildt@protonmail.com>
2019-09-30 14:33:51 +0200

    Complete Big Num

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,4 +1,7 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
 
-# YOUR CODE HERE
+TwoPower65536 = 2**65536
+
+print(TwoPower65536)
+print ("In the JS console, we get 'infinity'")
