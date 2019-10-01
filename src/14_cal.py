efd405f5186c847b56554d7667419f94c4404ab8
rui hildt <ruihildt@protonmail.com>
2019-10-01 18:01:01 +0200

    Complete Calendar

--- src/14_cal.py
+++ src/14_cal.py
@@ -9,11 +9,14 @@ and does the following:
  - If the user doesn't specify any input, your program should
    print the calendar for the current month. The 'datetime'
    module may be helpful for this.
+
  - If the user specifies one argument, assume they passed in a
    month and render the calendar for that month of the current year.
+
  - If the user specifies two arguments, assume they passed in
    both the month and the year. Render the calendar for that
    month and year.
+
  - Otherwise, print a usage statement to the terminal indicating
    the format that your program expects arguments to be given.
    Then exit the program.
@@ -22,3 +25,23 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+month = datetime.now().month
+year = datetime.now().year
+
+if (len(sys.argv) > 3 or month < 1 or month > 12 or year < 1970 or (not isinstance(month, int)) or (not isinstance(year, int))):
+    print(f"**Input Error**\n\nPlease enter the month and year as arguments in the following format `MM YYYY` \nor `MM` to get the calendar for the current year. \n\nExample 1: \"9 1999\" will show you the calendar for September 1999\nExample 2: \"1\" will show you the calendar for January {year}\n")
+    sys.exit()
+
+if len(sys.argv) > 1:
+    month = int(sys.argv[1])
+    year = year
+
+if len(sys.argv) > 2:
+    month = int(sys.argv[1])
+    year = int(sys.argv[2])
+
+def calendarize(month = month, year = year):
+  print(calendar.month(year, month))
+
+calendarize(month, year)
